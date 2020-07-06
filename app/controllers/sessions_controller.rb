d0ef667f27ef5f22123a3fb54a8dedab9a2bbf4c
Dennis <Dennis.portillo@yale.edu>
2020-07-06 13:08:03 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end 
+
+  def create
+      if !!params[:name] && !params[:name].empty?
+          session[:name] = params[:name]
+          redirect_to 'sessions/welcome'
+      else 
+          redirect_to '/'
+      end 
+  end
+
+  def welcome
+  end 
+
+  def destroy
+      session.delete :name
+  end 
+
+
+end
